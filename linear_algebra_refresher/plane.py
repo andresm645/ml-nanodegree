d7c0547df981625f5ab012c9b176cf60049bfd5a
Andres Mata <mataman@amazon.com>
2017-10-10 16:09:15 -0600

    Add is_parallel_to and overload == operator for plane.py. Also using vector.coordinates instead of just the vector in calling first_nonzero_index.

--- linear_algebra_refresher/plane.py
+++ linear_algebra_refresher/plane.py
@@ -30,7 +30,7 @@ class Plane(object):
             c = self.constant_term
             basepoint_coords = ['0']*self.dimension
 
-            initial_index = Plane.first_nonzero_index(n)
+            initial_index = Plane.first_nonzero_index(n.coordinates)
             initial_coefficient = n[initial_index]
 
             basepoint_coords[initial_index] = c/initial_coefficient
@@ -69,7 +69,7 @@ class Plane(object):
         n = self.normal_vector
 
         try:
-            initial_index = Plane.first_nonzero_index(n)
+            initial_index = Plane.first_nonzero_index(n.coordinates)
             terms = [write_coefficient(n[i], is_initial_term=(i == initial_index)) + 'x_{}'.format(i+1)
                      for i in range(self.dimension) if round(n[i], num_decimal_places) != 0]
             output = ' '.join(terms)
@@ -87,6 +87,31 @@ class Plane(object):
 
         return output
 
+    def __eq__(self, other):
+        if self.normal_vector.is_zero():
+            if not other.normal_vector.is_zero():
+                return False
+            else:
+                diff = self.constant_term - other.constant_term
+                return MyDecimal(diff).is_near_zero()
+        elif other.normal_vector.is_zero():
+            return False
+
+        if not self.is_parallel_to(other):
+            return False
+
+        x = self.basepoint
+        y = other.basepoint
+        connection = y.minus(x)
+
+        return connection.is_orthogonal_to(self.normal_vector)
+
+    def is_parallel_to(self, other):
+        n1 = self.normal_vector.normalized()
+        n2 = other.normal_vector.normalized()
+
+        return n1.is_parallel_to(n2)
+
     @staticmethod
     def first_nonzero_index(iterable):
         for k, item in enumerate(iterable):
@@ -98,3 +123,51 @@ class Plane(object):
 class MyDecimal(Decimal):
     def is_near_zero(self, eps=1e-10):
         return abs(self) < eps
+
+
+def main():
+    first = Vector([-0.412, 3.806, 0.728])
+    k1 = -3.46
+    second = Vector([1.03, -9.515, -1.82])
+    k2 = 8.65
+
+    a = Plane(first, k1)
+    b = Plane(second, k2)
+    if a == b:
+        print '1st pair is equal'
+    elif a.is_parallel_to(b):
+        print '1st pair is parallel but not equal'
+    else:
+        print '1st pair is not parallel'
+
+    first = Vector([2.611, 5.528, 0.283])
+    k1 = 4.6
+    second = Vector([7.715, 8.306, 5.342])
+    k2 = 3.76
+
+    a = Plane(first, k1)
+    b = Plane(second, k2)
+    if a == b:
+        print '2nd pair is equal'
+    elif a.is_parallel_to(b):
+        print '2nd pair is parallel but not equal'
+    else:
+        print '2nd pair is not parallel'
+
+    first = Vector([-7.926, 8.625, -7.212])
+    k1 = -7.952
+    second = Vector([-2.642, 2.875, -2.404])
+    k2 = -2.443
+
+    a = Plane(first, k1)
+    b = Plane(second, k2)
+    if a == b:
+        print '3rd pair is equal'
+    elif a.is_parallel_to(b):
+        print '3rd pair is parallel but not equal'
+    else:
+        print '3rd pair is not parallel'
+
+
+if __name__ == "__main__":
+    main()
